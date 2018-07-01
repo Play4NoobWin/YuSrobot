bc45212205f136163dbd2b46827c485948d86180
VYCKTOR STARK <huho387@gmail.com>
2018-07-01 19:59:31 -0400

    uploading the keyboards and update main fie

---
+++ keyboard.py
@@ -0,0 +1,26 @@
+import settings
+start = { "inline_keyboard": [
+  [{"url": 'https://t.me/{}?startgroup=start'.format(settings.BOT_USERNAME), "text": '👥 Add in group'},
+   {"url": 'https://t.me/{}'.format(settings.CHANNEL), "text": '👁‍🗨 Official Channel'}],
+  [{"callback_data": "plugin_all","text": '⚙ Command'}]]
+}
+plugin_all = { "inline_keyboard": [
+  [{"callback_data": "plugin_extra", "text": "🖇 Extra"}],
+  [{"callback_data": "plugin_utility", "text": "🛠 utility"}],
+  [{"callback_data": "plugin_entertainment","text": "💈 entertainment"}]]
+}
+plugin_extra = { "inline_keyboard": [
+  [{"callback_data": "plugin_utility", "text": "🛠 utility"}],
+  [{"callback_data": "plugin_entertainment","text": "💈 entertainment"}],
+  [{"callback_data": "plugin_all","text": "🔙"}]]
+}
+plugin_utility = { "inline_keyboard": [
+  [{"callback_data": "plugin_extra", "text": "🖇 Extra"}],
+  [{"callback_data": "plugin_entertainment","text": "💈 entertainment"}],
+  [{"callback_data": "plugin_all","text": "🔙"}]]
+}
+plugin_entertainment = { "inline_keyboard": [
+  [{"callback_data": "plugin_extra", "text": "🖇 Extra"}],
+  [{"callback_data": "plugin_utility", "text": "🛠 utility"}],
+  [{"callback_data": "plugin_all","text": "🔙"}]]
+}
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-import settings, argparse, telepot, re, plugins, sys
+import settings, argparse, telepot, re, plugins, sys, json, keyboard, gtts
 api = settings.api
 plugins.loadplugins()
 def viewer(msg):
