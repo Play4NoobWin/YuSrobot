d1c3732861952e6262d1e6c003c5a7e3cc901644
VYCKTOR STARK <huho387@gmail.com>
2018-07-01 13:49:11 -0400

    improving config

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-#-*- coding: utf-8 -*-
-from imports import *
-bot = telepot.Bot('Add bot token')
-#Add support group
-Mainchat = -100000000
+import telepot
+SECRET_KEY = "add your secret bot token"
+api = telepot.Bot(SECRET_KEY)
+IDBOT = SECRET_KEY[:9]
+SUPPORT = "-1001279317339"
+BOT = api.getMe()
+BOT_NAME = BOT['first_name']
+BOT_USERNAME = '@{}'.format(BOT['username'])
+SUDO = 1234556789
+SUPPORT = 1234556789
