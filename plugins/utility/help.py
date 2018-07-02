47a3171fc2e2b4170dca7df6de5929788e285318
VYCKTOR STARK <huho387@gmail.com>
2018-07-01 20:03:01 -0400

    updating help

--- plugins/utility/help.py
+++ plugins/utility/help.py
@@ -1,59 +1,46 @@
-from main import api, plugins, telepot
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-list_ = '📝 Commands List: {}\n send <code>/help type name|number</code>: to see how the command works.'
-def plugi(type_p=None, cmd_=None):
+from main import api, plugins, telepot, json, keyboard
+def plugi(typeplugin=None, cmd_=None):
+	list_ = '📝 Commands List: \n{}\nend'
 	lista = []
-	if type_p == "all": type_p = plugins.plugins_all
-	elif type_p == "entertainment": type_p = plugins.plugin_entertainment
-	elif type_p == "utility": type_p = plugins.plugin_utility
-	elif type_p == "extra": type_p = plugins.plugin_extra
-	for aPlug in type_p:
-		if not aPlug["sudo"] == True and not aPlug["name"] == 'start':
-				lista.append(aPlug)
 	x = '\n'
+	if typeplugin == "plugin_all": plugin = plugins.plugins_all
+	elif typeplugin == "plugin_entertainment": plugin = plugins.plugin_entertainment
+	elif typeplugin == "plugin_utility": plugin = plugins.plugin_utility
+	elif typeplugin == "plugin_extra": plugin = plugins.plugin_extra
+	for aPlug in plugin:
+		if not aPlug["sudo"] == True and not aPlug["name"] == 'Start':
+				lista.append(aPlug)
 	for num,plug in enumerate(lista):
 			if cmd_ != None:
 				if cmd_.lower() == plug['name'].lower() or str(cmd_) ==str(num):
 					x = plug['usage']
 			else:
-				x = x+"{} {}\n".format(num,lista[num]['name'])
+				if typeplugin != "plugin_all": x = x+"{}\n".format(plug['usage'])
+				else: x = x+"{} - {}\n".format(num,lista[num]['name'])
+	if cmd_ == None: 
+		if typeplugin == "plugin_all": x = list_.format(x).replace('end','send <code>/help name|number</code> to see how the command works.')
+		if typeplugin != "plugin_all": x = list_.format(x).replace('end','')
 	return x
 def Function(msg, cmd):
-	if 'cb' in msg:
-		if 'plugin_entertainment' in cmd[0]:
-			api.editMessageText(telepot.message_identifier(msg), list_.format(plugi(type_p=plugins.plugin_entertainment)), reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
-				InlineKeyboardButton(text='utility', callback_data='plugin_utility'),
-				InlineKeyboardButton(text='entertainment', callback_data='plugin_entertainment')
-			],[InlineKeyboardButton(text='back', callback_data='plugin_all')]]))
-		elif 'plugin_extra' in cmd[0]:
-			api.editMessageText(telepot.message_identifier(msg), list_(plugi(type_p=plugins.plugin_extra)), reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
-				InlineKeyboardButton(text='utility', callback_data='plugin_utility'),
-				InlineKeyboardButton(text='entertainment', callback_data='plugin_entertainment')
-			],[InlineKeyboardButton(text='back', callback_data='plugin_all')]]))
-		elif 'plugin_utility' in cmd[0]:
-			api.editMessageText(telepot.message_identifier(msg), list_.format(plugi(type_p=plugins.plugin_utility)), reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
-				InlineKeyboardButton(text='extra', callback_data='plugin_extra'),
-				InlineKeyboardButton(text='entertainment', callback_data='plugin_entertainment')
-			],[InlineKeyboardButton(text='back', callback_data='plugin_all')]]))
-		elif 'plugin_all' in cmd[0]:
-			api.editMessageText(telepot.message_identifier(msg), list_.format(plugi(type_p=plugins.plugins_all)).replace('type',''), reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
-				InlineKeyboardButton(text='extra', callback_data='plugin_extra'),
-				InlineKeyboardButton(text='entertainment', callback_data='plugin_entertainment')
-			],[InlineKeyboardButton(text='utility', callback_data='plugin_utility')]]))
-	elif (len(cmd) == 1):
-		return list_.format(plugi(type_p=plugins.plugins_all)).replace('type','')
+	tuple_id = (msg['chat']['id'], msg['message_id'])
+	if 'plugin_entertainment' in cmd[0] and 'cb' in msg:
+		try: api.editMessageText(tuple_id, plugi(cmd[0]), parse_mode='HTML',reply_markup=json.dumps(keyboard.plugin_entertainment))
+		except: return False
+	elif 'plugin_extra' in cmd[0] and 'cb' in msg:
+		try: api.editMessageText(tuple_id, plugi(cmd[0]), parse_mode='HTML',reply_markup=json.dumps(keyboard.plugin_extra))
+		except: return False
+	elif 'plugin_utility' in cmd[0] and 'cb' in msg:
+		try: api.editMessageText(tuple_id, plugi(cmd[0]), parse_mode='HTML', reply_markup=json.dumps(keyboard.plugin_utility))
+		except: return False
+	elif 'plugin_all' in cmd[0] and 'cb' in msg:
+		try: api.editMessageText(tuple_id, plugi(cmd[0]), parse_mode='HTML',reply_markup=json.dumps(keyboard.plugin_all))
+		except: return False
+	elif (len(cmd) == 1): return plugi('plugin_all')
 	else:
-		if (len(cmd) == 3):
-			try:
-				return plugi(type_p=cmd[1], cmd_=cmd[2])
-			except Exception as error:
-				return 'You typed something invalid.'
-		else:
-			try:
-				return plugi(type_p=plugins.plugins_all, cmd_=cmd[1])
-			except Exception as error:
-				print(error)
-				return 'You typed something invalid.'
+		try:
+			if (len(cmd) == 3): return plugi('plugin_{}'.format(cmd[1]), cmd_=cmd[2])
+			else: return plugi('plugin_all', cmd_=cmd[1])
+		except Exception as error: return 'You typed something invalid.'
 plugin = {
 	'patterns': [
 		"^[/!](help)$",
