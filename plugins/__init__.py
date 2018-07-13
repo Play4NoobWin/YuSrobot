0ea1325bc3fe00ce652a66d5c812024f94d0c87c
Vycktor Hugo Stark <huho387@gmail.com>
2018-07-13 12:34:40 -0400

    Updating system of plugins
    + organizing system
    + adding value recognition system, so you do not have to keep repeating the line
    - removing duplicity and unnecessary variables
    + adding return system, to return the quantity of loaded plugins

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,33 +1,25 @@
 __all__= ['loadplugins']
-def loadplugins():
-		import os, re
+def values_plugin(path):
+		import os, re, time
+		pluginsFile = []
 		curPath = os.path.dirname(os.path.realpath(__file__))
+		_path = "/{}/".format(path)
+		path_ = "/{}".format(path)
+		Files = [curPath + _path + f for f in os.listdir(curPath + path_) if re.search('^.+\.py$', f)]
+		for files in Files:
+			values = {}
+			with open(files, encoding='utf-8') as file:
+				code = compile(file.read(), files, 'exec')
+				exec(code, values)
+				pluginsFile.append(values['plugin'])
+		return pluginsFile
+def loadplugins():
 		global plugins_all, plugin_admin, plugin_utility, plugin_entertainment
 		plugins_all = []
-		plugin_admin = []
-		plugin_utility = []
-		plugin_entertainment = []
-		pluginFiles_admin = [curPath + "/admin/" + f for f in os.listdir(curPath + "/admin") if re.search('^.+\.py$', f)]
-		for file in pluginFiles_admin:
-			values = {}
-			with open(file, encoding='utf-8') as f:
-				code = compile(f.read(), file, 'exec')
-				exec(code, values)
-			plugin_admin.append(values['plugin'])
-			plugins_all.append(values['plugin'])
-		pluginFiles_utility = [curPath + "/utility/" + f for f in os.listdir(curPath + "/utility") if re.search('^.+\.py$', f)]
-		for file in pluginFiles_utility:
-			values = {}
-			with open(file, encoding='utf-8') as f:
-				code = compile(f.read(), file, 'exec')
-				exec(code, values)
-			plugin_utility.append(values['plugin'])
-			plugins_all.append(values['plugin'])
-		pluginFiles_entertainment = [curPath + "/entertainment/" + f for f in os.listdir(curPath + "/entertainment") if re.search('^.+\.py$', f)]
-		for file in pluginFiles_entertainment:
-			values = {}
-			with open(file, encoding='utf-8') as f:
-				code = compile(f.read(), file, 'exec')
-				exec(code, values)
-			plugin_entertainment.append(values['plugin'])
-			plugins_all.append(values['plugin'])
+		plugin_admin = values_plugin('admin')
+		plugins_all += plugin_admin
+		plugin_utility = values_plugin('utility')
+		plugins_all += plugin_utility
+		plugin_entertainment = values_plugin('entertainment')
+		plugins_all += plugin_entertainment
+		return len(plugins_all), len(plugin_admin), len(plugin_utility), len(plugin_entertainment) # returning the amount of plugins loaded
