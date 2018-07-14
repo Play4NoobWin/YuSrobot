bf5662a44b3ddb516cb920784695557d1057b112
Player4NoobWinner <adilfodaum@gmail.com>
2018-07-14 12:00:31 +0000

    adding reader for callback

--- keyboard.py
+++ keyboard.py
@@ -1,25 +1,34 @@
 import settings
-start = { "inline_keyboard": [
+__all__ = ['loadkeyboard', 'start', 'plugin_all', 'plugin_admin', 'plugin_utility', 'plugin_entertainment']
+def loadkeyboard(key):
+  import json
+  global keyboardLIST
+  if key in __all__:
+    keyboard = json.dumps(keyboardLIST[key])
+    return keyboard
+  else: return False
+keyboardLIST = dict()
+keyboardLIST['start'] = { "inline_keyboard": [
   [{"url": 'https://t.me/{}?startgroup=start'.format(settings.BOT_USERNAME), "text": '👥 Add in group'},
    {"url": 'https://t.me/{}'.format(settings.CHANNEL), "text": '👁‍🗨 Official Channel'}],
   [{"callback_data": "plugin_all","text": '⚙ Command'}]]
 }
-plugin_all = { "inline_keyboard": [
+keyboardLIST['plugin_all'] = { "inline_keyboard": [
   [{"callback_data": "plugin_admin", "text": "👤 admin"}],
   [{"callback_data": "plugin_utility", "text": "🛠 utility"}],
   [{"callback_data": "plugin_entertainment","text": "💈 entertainment"}]]
 }
-plugin_admin = { "inline_keyboard": [
+keyboardLIST['plugin_admin'] = { "inline_keyboard": [
   [{"callback_data": "plugin_utility", "text": "🛠 utility"}],
   [{"callback_data": "plugin_entertainment","text": "💈 entertainment"}],
   [{"callback_data": "plugin_all","text": "🔙"}]]
 }
-plugin_utility = { "inline_keyboard": [
+keyboardLIST['plugin_utility'] = { "inline_keyboard": [
   [{"callback_data": "plugin_admin", "text": "👤 admin"}],
   [{"callback_data": "plugin_entertainment","text": "💈 entertainment"}],
   [{"callback_data": "plugin_all","text": "🔙"}]]
 }
-plugin_entertainment = { "inline_keyboard": [
+keyboardLIST['plugin_entertainment'] = { "inline_keyboard": [
   [{"callback_data": "plugin_admin", "text": "👤 admin"}],
   [{"callback_data": "plugin_utility", "text": "🛠 utility"}],
   [{"callback_data": "plugin_all","text": "🔙"}]]
